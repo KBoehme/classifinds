07e71bdda40579714e9fd7b8cbb1d8d23fe03cea
KBoehme <kevinlboehme@gmail.com>
2016-05-15 23:49:33 -0600

    Started using search url. Easier, works better. Also added texting feature. Better logging.

--- classifinds.py
+++ classifinds.py
@@ -12,11 +12,29 @@ import os
 from datetime import datetime, timedelta
 import logging
 from logging.handlers import RotatingFileHandler
+try:
+    # Python 3
+    from urllib.parse import urlparse, parse_qs
+except ImportError:
+    # Python 2
+    from urlparse import urlparse, parse_qs
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
+
+# Use sms gateway provided by mobile carrier:
+# at&t:     number@mms.att.net
+# t-mobile: number@tmomail.net
+# verizon:  number@vtext.com
+# sprint:   number@page.nextel.com
+TEXT_MESSAGE_MAP = {
+	'at&t':     'mms.att.net',
+	't-mobile': 'tmomail.net',
+	'verizon':  'vtext.com',
+	'sprint':   'page.nextel.com'
+}
 
 """
 Find some cool stuff.
@@ -26,16 +44,18 @@ base = "" #Add base of config.ini file and things.json...
 
 config.read(base + 'config.ini')
 ourthings = json.load(open(base + 'things.json'))
-params = {
-	"cmd": "list",
-	"c": "500",
-	"o": "0",
-	"min": "0",
-	"max": "100",
-	"d": "20",
-	"srt": "Most+recent",
-	"slr": "Private+sellers"
+
+TEXT_MESSAGE = "I think I just klassifound something you might like. Check your email."
+
+URL_TO_API_MAP = {
+	"max_price": "max",
+	"min_price": "min",
+	"search": "s",
+	"nid": "category",
+	"zip": "z",
+	"distance": "d"
 }
+
 # Set up a specific logger with our desired output level
 klassifinds_log = logging.getLogger('MyLogger')
 klassifinds_log.setLevel(logging.INFO)
@@ -85,27 +105,39 @@ def process_ad(ad, keywords):
 
 	return return_string
 
+def prepare_query(query):
+	prepared_query = {}
+	for old_key, new_key in URL_TO_API_MAP.items():
+		if old_key in query:
+			prepared_query[new_key] = query[old_key]
+	prepared_query["cmd"] = "list"
+	return prepared_query
+
+
 def handle_things(email, things):
 	klassifinds_log.info("Checking {} stuff".format(email))
 	# Create message container.
 	email_summary = MIMEMultipart('related')
 	email_summary['Subject'] = "Klassifind winners"
-	email_summary['From'] = config.get('EMAIL', 'email')
+	email_summary['From'] = config.get('SECRETS', 'email')
 	email_summary['To'] = email
 	email_contents = []
 	total_hits = 0
-	for cool_thing, info in things.items():
-
-		params['s'] = cool_thing
-		klassifinds_log.info("\t- {}".format(cool_thing))
-		max_price = info['max']
+	send_text = False
+	for search_url, info in things.items():
 		keywords = info['keywords']
+		text_person = info['text']
 
-		r = requests.post(config.get('PARAMS', 'api_base'), data=params)
+		query = parse_qs( urlparse(search_url).query )
+		query = prepare_query(query)
+		cool_thing = query['s']
+
+		r = requests.post(config.get('PARAMS', 'api_base'), data=query)
 		hitresults = json.loads(r.text)
 		now = datetime.now()
-		last_run = now - timedelta(hours = 1)
-		promising_ads = [ad for ad in hitresults if (datetime.fromtimestamp(ad['display_time']) > last_run) and ad['image'] and float(ad['price']) < max_price ]
+		last_run = now - timedelta(minutes = 5)
+		promising_ads = [ad for ad in hitresults if (datetime.fromtimestamp(ad['display_time']) > last_run) and ad['image'] ]
+		klassifinds_log.info("\t- {} - Found {} ads ({} promising)".format(cool_thing, len(hitresults), len(promising_ads)))
 		hits = 0
 		ad_hit_contents = []
 		for ad in promising_ads:
@@ -114,6 +146,9 @@ def handle_things(email, things):
 			if result:
 				hits += 1
 				ad_hit_contents.append(result)
+				if send_text:
+					klassifinds_log.info('Found Text worthy Hit!')
+					send_text = True
 		if hits:
 			email_contents.append("<h2>{}</h2>".format(' '.join(cool_thing.split('+')).title()))
 			email_contents.extend(ad_hit_contents)
@@ -121,19 +156,27 @@ def handle_things(email, things):
 		total_hits += hits
 	html = ''.join(email_contents)
 	email_summary.attach(MIMEText(html, 'html'))
-	return total_hits, email_summary
+	return total_hits, email_summary, send_text
 
 def klassifinds():
 	klassifinds_log.info(datetime.utcnow())
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
-	server.login(config.get('EMAIL', 'email'), config.get('EMAIL', 'key'))
+	server.login(config.get('SECRETS', 'email'), config.get('SECRETS', 'key'))
 
-	for email, things in ourthings.items():
-		total_hits, email_summary = handle_things(email, things)
+	for name, info in ourthings.items():
+		email = info['email']
+		phone = info['phone']
+		carrier = info['carrier']
+		things = info['things']
+
+		total_hits, email_summary, send_text = handle_things(email, things)
 		if total_hits and not DEBUG:
 			klassifinds_log.info("Sending {} Klassifind winners to {}.".format(total_hits, email))
-			server.sendmail(config.get('EMAIL', 'email'), email, email_summary.as_string())
+			server.sendmail(config.get('SECRETS', 'email'), email, email_summary.as_string())
+			if send_text:
+				klassifinds_log.info("Sending text message to {}.".format(phone))
+				server.sendmail( config.get('SECRETS', 'email'), '{}@{}'.format(phone, TEXT_MESSAGE_MAP[carrier]), TEXT_MESSAGE)
 	klassifinds_log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 if __name__ == '__main__':
 	klassifinds()
